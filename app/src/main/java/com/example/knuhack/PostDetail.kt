6cfb1024f9ac07c2129f5adaf7a97d554b5cf242
hyun.min.han <um4825@gmail.com>
2021-07-23 01:47:31 +0900

    FIX : 댓글, 대댓글 누를시 팝업창 안뜨는 버그 해결

--- app/src/main/java/com/example/knuhack/PostDetail.kt
+++ app/src/main/java/com/example/knuhack/PostDetail.kt
@@ -1,6 +1,7 @@
 package com.example.knuhack
 
 import android.app.AlertDialog
+import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
@@ -19,57 +20,83 @@ import retrofit2.Response
 
 
 class PostDetail : AppCompatActivity() {
-    val items : ArrayList<CustomAdapter.AdapterItem> = ArrayList<CustomAdapter.AdapterItem>()
     val mContext  = this
-    lateinit var listView: ListView
 
+    // user data
     var userId : Long = -1
     lateinit var userNickname : String
+
+    // board data
+    val items : ArrayList<CustomAdapter.AdapterItem> = ArrayList<CustomAdapter.AdapterItem>()
+    lateinit var content: String
+    lateinit var tile: String
+    lateinit var author: String
+    var boardId : Long = -1
+
+    // view
+    lateinit var listView: ListView
+
+    // board
+    lateinit var commentTextView : EditText
+    lateinit var titleTextView : TextView
+    lateinit var detailTextView : TextView
+    lateinit var authorTextView: TextView
+
+
+    // button
+    lateinit var writeCommentBtn : Button
+    lateinit var toAuthorBtn : ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post_detail)
 
-        val boardId = intent.getLongExtra("boardId",0)
+        setDataWithIntent()
+        findViews()
+        setClickListeners()
+
+        getCommentList(boardId)
+    }
+
+    private fun setDataWithIntent() {
+        boardId = intent.getLongExtra("boardId",0)
         userId = intent.getLongExtra("userId", -1)
         intent.getStringExtra("nickname")?.let { userNickname = it }
-        
-        val content = intent.getStringExtra("content") // Intent에서 Key를 email로 가지고 있는 값 가져오기
-        val title= intent.getStringExtra("title")
-        val author = intent.getStringExtra("author")
 
-        val commentText = findViewById<EditText>(R.id.commentEdit) as EditText
-        val text1 = findViewById<TextView>(R.id.postTitle) as TextView
-        text1.setText(title)
-        val text2 = findViewById<TextView>(R.id.detailContent) as TextView
-        text2.setText(content)
-        val text3 = findViewById<TextView>(R.id.writer_postDetail) as TextView
-        text3.setText(author)
+        intent.getStringExtra("content")?.let { content = it }  // Intent에서 Key를 email로 가지고 있는 값 가져오기
+        intent.getStringExtra("title")?.let { title = it }
+        intent.getStringExtra("author")?.let { author = it }
+    }
 
-        val commentbtn = findViewById<Button>(R.id.writeCommentBtn) as Button
+    private fun findViews(){
+        commentTextView = findViewById<EditText>(R.id.commentEdit) as EditText
+        titleTextView = findViewById<TextView>(R.id.postTitle) as TextView
+        titleTextView.setText(title)
+        detailTextView= findViewById<TextView>(R.id.detailContent) as TextView
+        detailTextView.setText(content)
+        authorTextView = findViewById<TextView>(R.id.writer_postDetail) as TextView
+        authorTextView.setText(author)
 
-        commentbtn.setOnClickListener {
-            val content = commentText.text.toString().trim { it <= ' ' }
+        listView = findViewById<ListView>(R.id.commentlistview)
+        writeCommentBtn = findViewById<Button>(R.id.writeCommentBtn)
+        toAuthorBtn = findViewById<ImageButton>(R.id.menu_btn_postDetail)
+    }
+
+    private fun setClickListeners(){
+
+
+        writeCommentBtn.setOnClickListener {
+            val content = commentTextView.text.toString().trim { it <= ' ' }
             Log.i("가져온 텍스트 : ", content)
             if (userNickname != null) {
-                writeComment(boardId, userNickname, content, commentText)
+                writeComment(boardId, userNickname, content, commentTextView)
             }
         }
 
-        listView = findViewById<ListView>(R.id.commentlistview)
-
-        getCommentList(boardId)
-        
-        listView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
-            setCommentDialog(items[position])
-        }
-
-        text3.setOnClickListener {
-            setAuthorDialog(it.toString())
+        toAuthorBtn.setOnClickListener {
+            setAuthorDialog(author)
         }
     }
-
-
 
     private fun setAuthorDialog(author: String){
         val oItems = listOf<CharSequence>("프로필 보기", "쪽지 보내기").toTypedArray()
@@ -83,6 +110,7 @@ class PostDetail : AppCompatActivity() {
     }
 
     private fun setCommentDialog(item : CustomAdapter.AdapterItem) {
+
         val oItems = listOf<CharSequence>("프로필 보기", "쪽지 보내기", "대댓글 작성", "수정", "삭제").toTypedArray()
         val oDialog = AlertDialog.Builder(mContext, android.R.style.Theme_DeviceDefault_Light_Dialog_Alert)
         oDialog.setTitle("무엇을 하시겠어요?").setItems(oItems, DialogInterface.OnClickListener { dialog, which ->
@@ -135,7 +163,7 @@ class PostDetail : AppCompatActivity() {
         Toast.makeText(mContext, "delete", Toast.LENGTH_LONG).show()
     }
 
-    class CustomAdapter(private val items: MutableList<AdapterItem>) : BaseAdapter() {
+    class CustomAdapter(private val context: PostDetail, private val items: MutableList<AdapterItem>) : BaseAdapter() {
         data class AdapterItem(
             val author:String,
             val content:String,
@@ -158,6 +186,10 @@ class PostDetail : AppCompatActivity() {
                 val content = view.findViewById<TextView>(R.id.contents_comment)
                 content.text = items[position].content
 
+                view.findViewById<ImageButton>(R.id.menu_btn_comment).setOnClickListener {
+                    context.setCommentDialog(items[position])
+                }
+
                 return view
             }
             else
@@ -167,6 +199,10 @@ class PostDetail : AppCompatActivity() {
                 author.text = items[position].author
                 val content = view.findViewById<TextView>(R.id.content_reply)
                 content.text = items[position].content
+
+                view.findViewById<ImageButton>(R.id.menu_btn_reply).setOnClickListener {
+                    context.setCommentDialog(items[position])
+                }
 
                 return view
             }
@@ -193,7 +229,7 @@ class PostDetail : AppCompatActivity() {
                     }
 
                     //        어답터 설정
-                    listView.adapter = CustomAdapter(items)
+                    listView.adapter = CustomAdapter(mContext, items)
                 }
             }
 
