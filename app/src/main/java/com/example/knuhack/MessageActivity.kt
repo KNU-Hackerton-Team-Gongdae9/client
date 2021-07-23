5757d8c63c475acd513dcbcb7edb620553d06589
hyodonglee <good79797979@naver.com>
2021-07-23 11:27:25 +0900

    사이드바 간소화, 연결, 글작성 연동

--- app/src/main/java/com/example/knuhack/MessageActivity.kt
+++ app/src/main/java/com/example/knuhack/MessageActivity.kt
@@ -7,9 +7,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.knuhack.dto.ApiResult
 import com.example.knuhack.entity.Message
+import com.google.android.material.navigation.NavigationView
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -22,11 +25,55 @@ class MessageActivity : AppCompatActivity() {
 
     val mContext  = this
     lateinit var listView: ListView
+    //사이드바
+    lateinit var toggle : ActionBarDrawerToggle
+    lateinit var  drawerLayout : DrawerLayout
+    lateinit var navigationView: NavigationView
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.actvity_all_message_main)
+
+        //사이드바
+        drawerLayout  = findViewById(R.id.message_drawerLayout)
+        navigationView = findViewById(R.id.message_nav_view)
+
+        toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navigationView.setNavigationItemSelectedListener {
+            when(it.itemId)
+            {
+                R.id.menu_message -> {
+                    val intent = Intent(this, MessageActivity::class.java)
+                    intent.putExtra("nickname", userNickname)
+                    intent.putExtra("userId", userId)
+                    startActivity (intent)
+                }
+
+                R.id.menu_profile -> {
+                    val intent = Intent(this, ProfileActivity::class.java)
+                    intent.putExtra("nickname", userNickname)
+                    intent.putExtra("id", userId)
+                    startActivity (intent)
+                }
+
+                R.id.menu_home  -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.putExtra("category", "FREE")
+                    intent.putExtra("nickname", userNickname)
+                    intent.putExtra("userId", userId)
+                    startActivity (intent)
+                }
+
+            }
+            true
+        }
 
         intent.getStringExtra("nickname")?.let { userNickname = it }
         userId = intent.getLongExtra("userId", -1)
